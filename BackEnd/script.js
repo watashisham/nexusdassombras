11dae690e75b934c7b2afd0313b77bae90a272b2
Sham <wata5hi@proton.me>
2025-01-29 21:21:59 -0300

    img

--- BackEnd/script.js
+++ BackEnd/script.js
@@ -1,13 +1,25 @@
+let textos = [
+    "No mundo de Eryndor, um reino de dimensões interligadas por potais mágicos, existia uma grande força chamada Nexus das Sombras - Um ponto central onde todas as energias, tanto sombrias quanto luminosas, convergiam. O Nexus era o coração pulsante de Eryndor, mas também uma fonte de caos.",
+    "Por eras, o equilíbrio do Nexus era mantido por um grupo de guerreiras lendárias, cada uma representado uma faceta única do mundo: vampiras, elfas, necromantes, guerreiras e magas. Porém, o poder do Nexus começou a sair do controle quando a magia que o sustentava foi corrompida, dividindo Eryndor em facções em constante disputa.",
+    "Agora, cada guerreira deve lutar com seu povo para proteger, conquistar ou destruir o Nexus, seguindo suas próprias ambições e lealdades. Algumas lutam por justiça, outras pelo caos, e outras simplesmente por amor ou vingança."
+];
+
+let indiceTexto = 0; // Inicia com o primeiro texto
+
 document.getElementById('btn-avancar').addEventListener('click', function() {
     let caixaTexto = document.getElementById('caixa-texto');
     let instrucoes = document.getElementById('instrucoes');
 
-    // Animando a transição da caixa
+    // Se houver mais instruções, exibe a próxima
+    if (indiceTexto < textos.length) {
+        instrucoes.innerHTML = textos[indiceTexto];
+        indiceTexto++; // Avança para o próximo texto
+    }
+
+    // Animação de transição
     caixaTexto.style.transform = "scale(0.5)";
     
-    // Esperando 1 segundo para finalizar a animação antes de trocar o texto
     setTimeout(function() {
-        instrucoes.innerHTML = "Instrução 1: Prepare-se para a batalha!";
-        caixaTexto.style.transform = "scale(1)";  // Volta ao tamanho normal com animação
+        caixaTexto.style.transform = "scale(1)";
     }, 1000);
 });
